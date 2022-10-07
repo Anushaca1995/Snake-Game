59dd0c320467c300fa77835be9f34ccccfe31f72
Anusha Antony <anushaca1995@gmail.com>
2022-10-07 15:50:39 +0100

    Added variables and functionality to increase snake length

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-let snake_dir="";
-const getPressKey = (key) =>{
-    snake_dir=key;
-    console.log("snake_dir="+snake_dir);
+let canvas = document.querySelector(".canvas");
+let width = canvas.width;
+let height = canvas.height;
+let length = [];
+let food = {};
+let score = 0;
+let direction = 'right';
+let conf = {
+    cw: 8,
+    size: 4,
+    fps: 250
+};
+let context = "";
+let snake_dir = "";
+const getPressKey = (key) => {
+    snake_dir = key;
 }
+// Init Snake
+const initSnake = () => {
+    for (let index = 0; index < conf.size; index++) {
+    // Add Snake Cells
+        length.push({
+            x: index,
+            y: 0
+        });
+    }
+};
 
-console.log("snake_dirOut="+snake_dir);
+
+// Game Snake
+const gameSnake = () => {
+    let gamearea = document.querySelector(".canvas");
+    context = gamearea.getContext("2d");
+    initSnake();
+};
+gameSnake();
