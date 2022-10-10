bfd6d37ebc9a27009c23e20c6ee25a1369ec15de
Anusha Antony <anushaca1995@gmail.com>
2022-10-10 16:17:25 +0100

    Added restart functionality in snake game

--- script.js
+++ script.js
@@ -3,63 +3,68 @@ let width = canvas.width;
 let height = canvas.height;
 let length = [];
 let food = {};
+let flag;
 let score = 0;
+let pause=0;
 let direction = 'right';
 let conf = {
-    cw: 8,
-    size: 4,
-    fps: 250
+  cw: 8,
+  size: 4,
+  fps: 250
 };
-let context = "";
+let context="";
 let snake_dir = "";
 const getPressKey = (key) => {
-    snake_dir = key;
+  snake_dir = key;
 }
 // Init Snake
 const initSnake = () => {
-    for (let index = 0; index < conf.size; index++) {
+
+  // Itaration in Snake Conf Size
+  for (var i = 0; i < conf.size; i++) {
+
     // Add Snake Cells
-        length.push({
-            x: index,
-            y: 0
-        });
-    }
+    length.push({
+      x: i,
+      y: 0
+    });
+  }
 };
+
 // Init Food  
 const initFood = () => {
 
-    // Add food on stage
-    food = {
-      x: Math.round(Math.random() * (width - conf.cw) / conf.cw),
-      y: Math.round(Math.random() * (height - conf.cw) / conf.cw),
-    };
+  // Add food on stage
+  food = {
+    x: Math.round(Math.random() * (width - conf.cw) / conf.cw),
+    y: Math.round(Math.random() * (height - conf.cw) / conf.cw),
   };
-  // Restart Stage
+};
+
+// Restart Stage
 const restart = () => {
-    length = [];
-    food = {};
-    score = 0;
-    direction = 'right';
-    initSnake();
-    initFood();
-  };
-// Check Collision with walls
-const collision = (nx, ny) => {
-    if (nx == -1 || nx == (width / conf.cw) || ny == -1 || ny == (height / conf.cw)) {
-      return true;
-    }
-    return false;
-  }
-//Draw Stage
+  length = [];
+  food = {};
+  score = 0;
+  direction = 'right';
+  initSnake();
+  initFood();
+};
+const getRestart = () =>{
+  restart();
+}
+
+// Draw Stage
 const drawStage = () => {
   // Check Keypress And Set Stage direction
   let keyPress = snake_dir;
   if (typeof (keyPress) != 'undefined') {
     direction = keyPress;
   }
- // Draw purple Stage
+
+  // Draw blue Stage
   context.fillStyle = "#D6C0D4";
-  //context.fillRect(x,y,width,height);
+  //create filled rectangle context.fillRect(x,y,width,height);
   context.fillRect(0, 0, width, height);
 
   // Snake Position
@@ -81,12 +86,18 @@ const drawStage = () => {
       ny++;
       break;
   }
+
   // Check Collision
   if (collision(nx, ny) == true) {
-    //alert("Game Over");
+    if (flag){
+      console.log("Game over");
+      flag=0;
+    }
+    
     restart();
     return;
   }
+
   // Logic of Snake food
   if (nx == food.x && ny == food.y) {
     var tail = {
@@ -102,28 +113,43 @@ const drawStage = () => {
   }
   length.unshift(tail);
 
-// Draw Snake
+  // Draw Snake
   for (var i = 0; i < length.length; i++) {
     var cell = length[i];
     drawCell(cell.x, cell.y);
   }
+ // Draw Food
+ flag=1;
+  drawCell(food.x, food.y);
+  document.querySelector(".score").innerText="Score: "+score;
 };
+
 // Draw Cell
 const drawCell = (x, y) => {
-    context.fillStyle = '#071C02';
-    context.beginPath();
-    context.arc((x * conf.cw +6 ), (y * conf.cw + 6), 4, 0, 2 * Math.PI, false);
-    context.fill();
-  
-  };
+  context.fillStyle = '#071C02';
+  context.beginPath();
+  context.arc((x * conf.cw + 6 ), (y * conf.cw + 6), 4, 0, 2 * Math.PI, false);
+  context.fill();
+
+};
+// Check Collision with walls
+const collision = (nx, ny) => {
+  if (nx == -1 || nx == 37 || ny == -1 || ny == 19) {
+    return true;
+  } if (ny==19){
+    return true
+  }
+  return false;
+}
 
 // Game Snake
 const gameSnake = () => {
-    let gamearea = document.querySelector(".game-area");
-    context = gamearea.getContext("2d");
-    initSnake();
-    setInterval( () => {
-        drawStage();
-      }, conf.fps);
+  let gamearea = document.querySelector(".game-area");
+  context = gamearea.getContext("2d");
+  initSnake();
+  initFood();
+  setInterval( () => {
+    drawStage();
+  }, conf.fps);
 };
 gameSnake();
